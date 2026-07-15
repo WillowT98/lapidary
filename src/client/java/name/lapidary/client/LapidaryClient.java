d71c8de4bd5346b0c80d0be6e522b67a611149b4
WillowT98 <jamestilapia@gmail.com>
2026-07-15 12:08:23 -0700

    Create working Fabric development environment

---
+++ src/client/java/name/lapidary/client/LapidaryClient.java
@@ -0,0 +1,10 @@
+package name.lapidary.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LapidaryClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/name/lapidary/client/LapidaryDataGenerator.java
@@ -0,0 +1,11 @@
+package name.lapidary.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LapidaryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
